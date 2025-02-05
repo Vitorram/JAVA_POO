e102fa4a07ae2b0369ce2acedee2b0264e4f7a47
Vitorram <vitorramosmeneses11@gmail.com>
2025-02-05 20:39:52 -0300

    java

---
+++ aula01.java
@@ -0,0 +1,11 @@
+public class aula01{
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Hello word!!");
+        System.out.println("ola, ola");
+       
+    }
+    
+}
