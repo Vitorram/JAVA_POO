41e75d4e4b21be7ce749246f6d164a27e3cebb0e
Vitor rams <161902126+Vitorram@users.noreply.github.com>
2025-02-05 21:14:55 -0300

    Update aula01.java

--- aula01.java
+++ aula01.java
@@ -1,11 +1,18 @@
 public class aula01{
 
     public static void main(String[] args) {
-        // TODO code application logic here
         
         System.out.print("Hello word!!");
         System.out.println("ola, ola");
-       
+        String nome;
+        int idade;
+        float valor;
+        boolean vf;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite a sua idade: ");
+        idade = sc.nextInt();
+        System.out.println("Voce tem "+ idade +" anos,");
     }
     
 }
